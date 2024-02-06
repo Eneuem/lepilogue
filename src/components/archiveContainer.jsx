f67c23d69090ffe0bc5cfdfa48d3c3fc51d1cb91
Eneuem <nemozor@gmail.com>
2024-02-06 17:54:47 +0100

    yo

---
+++ src/components/archiveContainer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './archiveContainer.css';
+import image1 from '../assets/aemporter.jpg';
+import image2 from '../assets/brocoli.jpg';
+import image3 from '../assets/l2f.jpg';
+import image4 from '../assets/rudimentaire.jpg';
+import image5 from '../assets/tpplt.jpg';
+
+const images = [
+    { src: image1, link: 'https://example.com/image1' },
+    { src: image2, link: 'https://example.com/image2' },
+    { src: image3, link: 'https://example.com/image3' },
+    { src: image4, link: 'https://example.com/image4' },
+    { src: image5, link: 'https://example.com/image5' },
+];
+
+const ImageComponent = () => {
+    return (
+        <div className=''>
+            <div className=''>
+                <h2>Archives</h2>
+            </div>
+            <div className='figures'>
+                {images.map((image, index) => (
+                    <a key={index} href={image.link}>
+                        <img src={image.src} alt={`Image ${index}`} />
+                    </a>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default ImageComponent;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Footer from './components/footer.jsx'
 import MainContainer from './components/mainContainer.jsx'
+import ImageComponent from './components/archiveContainer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <MainContainer />
+    <ImageComponent />
     <Footer />
   </React.StrictMode>,
 )
